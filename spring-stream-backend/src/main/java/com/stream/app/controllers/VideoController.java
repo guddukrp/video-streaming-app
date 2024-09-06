2358ce7fe528b973eefe4e644811a8209c529c60
Guddu Kumar Prasad <91307403+guddukrp@users.noreply.github.com>
2024-09-06 14:26:52 +0530

    video stream backend file

---
+++ spring-stream-backend/src/main/java/com/stream/app/controllers/VideoController.java
@@ -0,0 +1,97 @@
+package com.stream.app.controllers;
+
+import com.stream.app.entities.Video;
+import com.stream.app.playload.CustomMessage;
+import com.stream.app.service.VideoService;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/videos")
+@CrossOrigin("*")
+public class VideoController {
+
+    private VideoService videoService;
+
+    public VideoController(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
+    //video upload
+    @PostMapping
+    public ResponseEntity<?> create(
+            @RequestParam("file")MultipartFile file,
+            @RequestParam("title") String title,
+            @RequestParam("description") String description
+            ){
+
+        Video video = new Video();
+        video.setTitle(title);
+        video.setDescription(description);
+        video.setVideoId(UUID.randomUUID().toString());
+
+        Video savedVideo = videoService.save(video, file);
+
+        if(savedVideo!=null){
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(video);
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(CustomMessage.builder()
+                            .message("Video not uploaded")
+                            .success(false)
+                            .build()
+                    );
+
+        }
+
+    }
+
+    //get all vidoes
+
+    @GetMapping
+    public List<Video> getAll(){
+        System.out.println(videoService.getAllVideo());
+        return videoService.getAllVideo();
+    }
+
+
+
+
+
+
+
+
+
+    //stream video  http://localhost:8080/api/v1/videos/stream/{videoId}
+
+    @GetMapping("/stream/{videoId}")
+    public ResponseEntity<Resource> stream(@PathVariable String videoId){
+        Video video = videoService.get(videoId);
+
+        String contentType = video.getContentType();
+        String filePath = video.getFilePath();
+
+        Resource resource =  new FileSystemResource(filePath);
+
+        if(contentType==null){
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(resource);
+
+    }
+}
